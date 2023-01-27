16f92382f764cff9a7c7eaa59ac682bd996f9467
trteel1 <tateelamino@gmail.com>
2023-01-27 11:46:47 +0300

    Completed assignment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-
 function QuestionForm(props) {
   const [formData, setFormData] = useState({
     prompt: "",
@@ -9,7 +8,6 @@ function QuestionForm(props) {
     answer4: "",
     correctIndex: 0,
   });
-
   function handleChange(event) {
     setFormData({
       ...formData,
@@ -19,7 +17,22 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: formData.prompt,
+        answers: [
+          formData.answer1,
+          formData.answer2,
+          formData.answer3,
+          formData.answer4,
+        ],
+        correctIndex: parseInt(formData.correctIndex),
+      }),
+    });
   }
 
   return (
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,13 +1,32 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteClick, onAnswerChange }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
+
+    
+        
+          
+    
+
+        
+    
+     function QuestionItem({ question }) {
+  
     <option key={index} value={index}>
       {answer}
-    </option>
+    </option>}
+    
   ));
+     
+  function handleDeleteClick() {
+    onDeleteClick(id);
+  }
+
+  function handleAnswerChange(event) {
+    onAnswerChange(id, parseInt(event.target.value));
+  }
 
   return (
     <li>
@@ -15,11 +34,12 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleAnswerChange}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
+
 
 export default QuestionItem;
